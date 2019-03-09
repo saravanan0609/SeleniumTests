49a81982d91ccf16e13af61ef7c8fc896fa83a01
sarav <sarav@DESKTOP-S536VJL>
2019-03-09 18:33:36 +0530

    new

--- Githubproject/src/test/java/example/NewTest.java
+++ Githubproject/src/test/java/example/NewTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;		
 public class NewTest {		
 	    private WebDriver driver;		
-		@Test				
+					
 		
 		@Test				
 		public void testEasy() {	
