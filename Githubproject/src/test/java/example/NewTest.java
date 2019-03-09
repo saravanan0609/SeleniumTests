33f5197b469cb45cd8e8da2d183da7e33a178eb8
sarav <sarav@DESKTOP-S536VJL>
2019-03-09 17:13:49 +0530

    second commit

--- Githubproject/src/test/java/example/NewTest.java
+++ Githubproject/src/test/java/example/NewTest.java
@@ -16,6 +16,12 @@ public class NewTest {
 			String title = driver.getTitle();				 
 			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
 		}	
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
 		@BeforeTest
 		public void beforeTest() {	
 			System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");	
